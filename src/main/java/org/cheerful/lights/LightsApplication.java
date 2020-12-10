c48877310d96364f0dc0dfe5760e23d0c8bc58c8
Okst1988 <chrisleonard.05@gmail.com>
2020-12-10 00:07:19 -0600

    Add build.gradle, index.html, index.js, IndexController.java, lights.css, and LightsApplication.java

---
+++ src/main/java/org/cheerful/lights/LightsApplication.java
@@ -0,0 +1,11 @@
+package org.cheerful.lights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LightsApplication.class, args);
+    }
+}
---
+++ src/main/resources/static/index.js
@@ -0,0 +1,5 @@
+function changeCssOnEl(){
+    let lights = $('.cheerful-light');
+    lights.toggleClass('green-light');
+    lights.toggleClass('red-light');
+}
